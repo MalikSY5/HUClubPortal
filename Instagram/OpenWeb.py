772d62836184902d20e77f9a080cd6bfaa15f5b7
Malik  Stewart <86385395+MalikSY5@users.noreply.github.com>
2022-03-19 09:28:40 -0400

    Update OpenWeb.py

--- Instagram/OpenWeb.py
+++ Instagram/OpenWeb.py
@@ -1,10 +1,11 @@
 from urllib import request
 from InstagramGrabber import Organization
 import webbrowser
-
+# Runs the function from InstagramGrabber
 database = Organization()
+# Currently a place holder. Further into project cycle this will be replaced with a function that grabs the post depending on what the user has entered.
 request = 'howardnsbe'
-
+# Grabs link and opens the post on a webrowser.
 for i in database:
     if i == request:
         webbrowser.open(database[i][0][0])
